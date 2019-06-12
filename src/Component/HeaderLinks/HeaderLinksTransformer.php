pesovo/shopsys

added shopsys/api bundle
- working endpoints
    - /api/v1/products (includes filter by uuids, pagination)
    - /api/v1/products/{uuid}
- using FOSRestBundle
- for finding products from facade is used query so the code is prepared for further extension by extending the ProductQuery and the query method ProductRepository::findByQuery()
- routes in project-base must be added for each version - that is a compromise between "routes works automatically when I add the bundle" and "user have to add all routes manually"
- api extension works form project-base in a way "what is extended is taken from project-base; what is not extended is taken from framework"

packages/api/src/Component/HeaderLinks/HeaderLinksTransformer.php
@@ -1,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\ApiBundle\Component\HeaderLinks;
+
+use Shopsys\FrameworkBundle\Component\Paginator\PaginationResult;
+use Symfony\Component\HttpFoundation\Request;
+
+class HeaderLinksTransformer
+{
+    /**
+     * @param \Shopsys\FrameworkBundle\Component\Paginator\PaginationResult $paginationResult
+     * @param string $baseUrl
+     * @return \Shopsys\ApiBundle\Component\HeaderLinks\HeaderLinks
+     */
+    public function fromPaginationResult(PaginationResult $paginationResult, string $baseUrl): HeaderLinks
+    {
+        $links = new HeaderLinks();
+
+        if (!$paginationResult->isFirst()) {
+            $firstUrl = $this->changeParameter($baseUrl, 'page', '1');
+            $previousUrl = $this->changeParameter($baseUrl, 'page', (string)$paginationResult->getPrevious());
+
+            $links = $links
+                ->add($firstUrl, 'first')
+                ->add($previousUrl, 'prev');
+        }
+
+        if (!$paginationResult->isLast()) {
+            $nextUrl = $this->changeParameter($baseUrl, 'page', (string)$paginationResult->getNext());
+            $lastUrl = $this->changeParameter($baseUrl, 'page', (string)$paginationResult->getPageCount());
+
+            $links = $links
+                ->add($nextUrl, 'next')
+                ->add($lastUrl, 'last');
+        }
+
+        return $links;
+    }
+
+    /**
+     * @param string $baseUrl
+     * @param string $parameter
+     * @param string $value
+     * @return string
+     */
+    protected function changeParameter(string $baseUrl, string $parameter, string $value): string
+    {
+        return Request::create($baseUrl, 'GET', [$parameter => $value])->getUri();
+    }
+}

shopsys/backend-api

src/Component/HeaderLinks/HeaderLinksTransformer.php
@@ -1,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\BackendApiBundle\Component\HeaderLinks;
+
+use Shopsys\FrameworkBundle\Component\Paginator\PaginationResult;
+use Symfony\Component\HttpFoundation\Request;
+
+class HeaderLinksTransformer
+{
+    /**
+     * @param \Shopsys\FrameworkBundle\Component\Paginator\PaginationResult $paginationResult
+     * @param string $baseUrl
+     * @return \Shopsys\BackendApiBundle\Component\HeaderLinks\HeaderLinks
+     */
+    public function fromPaginationResult(PaginationResult $paginationResult, string $baseUrl): HeaderLinks
+    {
+        $links = new HeaderLinks();
+
+        if (!$paginationResult->isFirstPage()) {
+            $firstUrl = $this->createUrlWithParameter($baseUrl, 'page', '1');
+            $previousUrl = $this->createUrlWithParameter($baseUrl, 'page', (string)$paginationResult->getPreviousPage());
+
+            $links = $links
+                ->add($firstUrl, 'first')
+                ->add($previousUrl, 'prev');
+        }
+
+        if (!$paginationResult->isLastPage()) {
+            $nextUrl = $this->createUrlWithParameter($baseUrl, 'page', (string)$paginationResult->getNextPage());
+            $lastUrl = $this->createUrlWithParameter($baseUrl, 'page', (string)$paginationResult->getPageCount());
+
+            $links = $links
+                ->add($nextUrl, 'next')
+                ->add($lastUrl, 'last');
+        }
+
+        return $links;
+    }
+
+    /**
+     * @param string $baseUrl
+     * @param string $parameter
+     * @param string $value
+     * @return string
+     */
+    protected function createUrlWithParameter(string $baseUrl, string $parameter, string $value): string
+    {
+        return Request::create($baseUrl, 'GET', [$parameter => $value])->getUri();
+    }
+}